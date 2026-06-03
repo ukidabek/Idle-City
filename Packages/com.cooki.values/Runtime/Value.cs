e6ad4098d703385b277f09eb8985ba1be1bda43c
Łukasz Dąbek <ukidabek@gmail.com>
2026-06-03 22:51:59 +0200

    [Code][Package] Bulk edit logic for variable added.

--- Packages/com.cooki.values/Runtime/Value.cs
+++ Packages/com.cooki.values/Runtime/Value.cs
@@ -5,6 +5,28 @@ namespace Values
 {
 	public abstract class BaseValue<T> : ScriptableObject
 	{
+		private class BulkEditScope<T> : IDisposable
+		{
+			private readonly BaseValue<T>  m_baseValue;
+			private readonly T m_value;
+			
+			public BulkEditScope(BaseValue<T> baseValue)
+			{
+				m_baseValue = baseValue;
+				m_value = baseValue.Value;
+				m_baseValue.m_isSilent = true;
+			}
+
+			public void Dispose()
+			{
+				m_baseValue.m_isSilent = false;
+				if (m_baseValue.m_value.Equals(m_value)) return;
+				m_baseValue.OnValueChanged.Invoke(m_baseValue.Value);
+			}
+		}
+
+		
+		private bool m_isSilent = false;
 		public event Action<T> OnValueChanged;
 
 		[SerializeField] protected T m_value;
@@ -15,8 +37,11 @@ namespace Values
 			{
 				if (m_value.Equals(value)) return;
 				m_value = value;
+				if(m_isSilent) return;
 				OnValueChanged?.Invoke(m_value);
 			}
 		}
+
+		public IDisposable BulkEdit() => new BulkEditScope<T>(this);
 	}
 }
