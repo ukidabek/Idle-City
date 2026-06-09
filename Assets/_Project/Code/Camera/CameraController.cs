48c6ec2314a3015ab8d5c83036dacba51f2d833b
Łukasz Dąbek <ukidabek@gmail.com>
2026-06-09 22:15:20 +0200

    [Code] Camera zoom added.

--- Assets/_Project/Code/Camera/CameraController.cs
+++ Assets/_Project/Code/Camera/CameraController.cs
@@ -1,5 +1,6 @@
 using System;
 using cookie.Logging;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
@@ -12,13 +13,19 @@ namespace _Project.CameraManagement
     {
         public Color Color { get; } = new Color(0.4f, 0.8f, 1f);
         [field: SerializeField] public LogMode Mode { get; private set; } = LogMode.All;
-        
+        [SerializeField] private CinemachineFollow m_cinemachineFallow = null;
+        [SerializeField, Min(0f)] private float m_zoomSpeed = 5f;
+        [SerializeField] private Vector3 m_minFov = Vector3.zero;
+        [SerializeField] private Vector3 m_maxFov = Vector3.zero;
+        [SerializeField, Range(0f,1f)] private float m_lerpValue = 0.5f;
+        [Space]
         [SerializeField, ReadOnly] private Camera m_camera = null;
         [SerializeField, ReadOnly] private EventSystem m_eventSystem = null;
         [Space]
         [SerializeField] private InputActionReference m_clickActionAsset = null;
         [SerializeField] private InputActionReference m_deltaActionAsset = null;
         [SerializeField] private InputActionReference m_positionActionAsset = null;
+        [SerializeField] private InputActionReference m_zoomActionAsset = null;
         [Space]
         [SerializeField] private Transform m_cameraTarget = null;
         [SerializeField, Min(0)] private float m_speed = 5f;
@@ -34,8 +41,24 @@ namespace _Project.CameraManagement
             m_eventSystem = EventSystem.current;
             m_deltaActionAsset.action.performed += ReadPosition;
             m_clickActionAsset.action.canceled += Select;
+            m_zoomActionAsset.action.performed += Zoom;
         }
 
+        private void OnDestroy()
+        {
+            m_deltaActionAsset.action.performed -= ReadPosition;
+            m_clickActionAsset.action.canceled -= Select;
+            m_zoomActionAsset.action.performed -= Zoom;
+        }
+
+        private void Zoom(InputAction.CallbackContext obj)
+        {
+            var input = obj.ReadValue<Vector2>().y;
+            m_lerpValue += m_zoomSpeed * -input * Time.deltaTime;
+            m_lerpValue = Mathf.Clamp01(m_lerpValue);
+            m_cinemachineFallow.FollowOffset = Vector3.Lerp(m_minFov, m_maxFov, m_lerpValue);
+        }
+        
         private void Select(InputAction.CallbackContext obj)
         {
             if (m_isOverUI) return;
@@ -61,5 +84,11 @@ namespace _Project.CameraManagement
         }
 
         private void Update() => m_isOverUI = m_eventSystem.IsPointerOverGameObject();
+
+
+        private void OnValidate()
+        {
+            Zoom(default);
+        }
     }
 }
