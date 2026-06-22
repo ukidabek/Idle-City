4a207af271453b41d5b87f176dd2fcfa5aa80ed7
Łukasz Dąbek <ukidabek@gmail.com>
2026-06-22 22:25:54 +0200

    [Code] Upgrades level assigning added.

---
+++ Assets/_Project/Code/Upgraids/Editor/UpgradeCollectionFiller.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Code.Upgrades
+{
+    public static class UpgradeCollectionFiller
+    {
+        public static readonly string UpgradeFilter = $"t:{nameof(Upgrade)}";
+        public static readonly string UpgradeCollectionFilter = $"t:{nameof(UpgradeCollection)}";
+        public static readonly BindingFlags BindingFlags = BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+        public static readonly FieldInfo UpgradesList = null;
+
+        static UpgradeCollectionFiller()
+        {
+            var type = typeof(UpgradeCollection);
+            UpgradesList = type.GetField("m_upgrades", BindingFlags);
+        }
+
+        [InitializeOnEnterPlayMode]
+        private static void OnEnterPlaymodeInEditor(EnterPlayModeOptions options)
+        {
+            if (options.HasFlag(EnterPlayModeOptions.DisableDomainReload))
+                return;
+
+            var upgrades = GetAllUpdates();
+            var upgradesCollection = GetUpgradeCollection();
+
+            UpgradesList.SetValue(upgradesCollection, upgrades);
+        }
+
+        public static Upgrade[] GetAllUpdates()
+        {
+            return AssetDatabase.FindAssets(UpgradeFilter)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Select(AssetDatabase.LoadAssetAtPath<Upgrade>)
+                .ToArray();
+        }
+
+        public static UpgradeCollection GetUpgradeCollection()
+        {
+            var guids = AssetDatabase.FindAssets(UpgradeCollectionFilter);
+            if (guids.Length > 1)
+            {
+                Debug.LogError($"There is more than one upgrade collection({nameof(UpgradeCollection)})!");
+            }
+
+            return AssetDatabase.LoadAssetAtPath<UpgradeCollection>(AssetDatabase.GUIDToAssetPath(guids[0]));
+        }
+    }
+
+    public class UpgradeCollectionFillerPreprocess : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+        }
+    }
+}
---
+++ Assets/_Project/Code/Upgraids/UpgradeCollection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Code.Upgrades
+{
+    [CreateAssetMenu(fileName = "Upgrades", menuName = "Upgrades/UpgradeCollection", order = 0)]
+    public class UpgradeCollection : ScriptableObject
+    {
+        [SerializeField] private Upgrade[] m_upgrades;
+
+        private readonly SortedDictionary<int, List<Upgrade>> m_upgradesLevels = new SortedDictionary<int, List<Upgrade>>();
+
+        private void OnEnable()
+        {
+#if UNITY_EDITOR
+            if (!UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode) return;
+#endif
+            if (m_upgrades == null || m_upgrades.Length == 0) return;
+            
+            foreach (var upgrade in m_upgrades)
+            {
+                var level = CalculateLevel(upgrade);
+
+                if (m_upgradesLevels.TryGetValue(level, out var list))
+                {
+                    list.Add(upgrade);
+                    continue;
+                }
+
+                m_upgradesLevels.Add(level, new List<Upgrade> { upgrade });
+            }
+
+            foreach (var pair in m_upgradesLevels)
+            {
+                Debug.Log($"{pair.Key} {pair.Value.Count}");
+            }
+        }
+
+        public static int CalculateLevel(Upgrade upgrade, int level = 0)
+        {
+            var maxLevel = level;
+            foreach (var dependency in upgrade.Dependencies)
+            {
+                var dependencyLevel = CalculateLevel(dependency.Upgrade, level + 1);
+                if (dependencyLevel > maxLevel)
+                    maxLevel = dependencyLevel;
+            }
+
+            return maxLevel;
+        }
+    }
+}
